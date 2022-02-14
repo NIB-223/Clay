905c83483b1a88088390e5cbda65802487665a8b
Robert <robertu.dev@gmail.com>
2022-02-13 22:26:55 -0600

    finished map and _testimonials

--- templates/landscaping/js/main.js
+++ templates/landscaping/js/main.js
@@ -1,5 +1,22 @@
+//Menu
 $(document).ready(function () {
     $('.menu__icon').click(function () {
         $('body').toggleClass('menu_shown');
     });
 });
+
+//Map
+function initMap() {
+    //location
+    const denver = { lat: 39.77, lng: -104.98 };
+    //centered map
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: denver,
+    });
+    //marker
+    const marker = new google.maps.Marker({
+        position: denver,
+        map: map,
+    });
+}
