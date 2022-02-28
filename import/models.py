25281ee21679a12b99124165a0928440a31f1344
quentin <q.volant@akenatechnologies.com>
2022-02-28 13:26:43 +0100

    Adds basic template model

--- import/models.py
+++ import/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Template(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    git_url = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
