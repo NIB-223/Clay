a036d846cd4f682808aae7e4cef1ba9b9a94e8f7
quentin <q.volant@akenatechnologies.com>
2022-02-28 13:40:55 +0100

    Updates git urls to unique

--- templating/models.py
+++ templating/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Template(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    git_url = models.CharField(max_length=255)
+    git_url = models.CharField(max_length=255, unique=True)
     author = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
