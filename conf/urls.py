3694d836622a63af37caea69ad93140ce9f6cf08
quentin <q.volant@akenatechnologies.com>
2022-02-28 13:58:54 +0100

    Adds basic templates app urls

--- conf/urls.py
+++ conf/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('template/', include('templating.urls')),
 ]
 
 if settings.DEBUG:
--- templating/views.py
+++ templating/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def listing(request):
+    return render(request, 'listing.html')
