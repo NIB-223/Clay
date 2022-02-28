d8a6263252800575b09eda87cffdcff2e4b271fa
quentin <q.volant@akenatechnologies.com>
2022-02-28 13:29:58 +0100

    Renames import app to templating

--- templating/apps.py
+++ templating/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ImportConfig(AppConfig):
+class TemplatingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'import'
+    name = 'templating'
