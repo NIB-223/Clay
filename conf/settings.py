d13a64067e4b1604b22b0182dadfe43bcac4cde5
Quentin Volant <quentin.volant@gmail.com>
2022-03-13 17:27:45 +0100

    Adds static folder

--- conf/settings.py
+++ conf/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "git_templates" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
