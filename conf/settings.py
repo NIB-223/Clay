0251c22651197d2982844726b178ff9aa67a8cef
Quentin Volant <quentin.volant@gmail.com>
2022-03-13 15:48:42 +0100

    Adds custom dir for git templates

--- conf/settings.py
+++ conf/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'git_templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
