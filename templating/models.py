729a93000114119796bd94f6fedd596c8775b36c
Quentin Volant <quentin.volant@gmail.com>
2022-03-13 15:52:58 +0100

    Auto adds slug for template model

--- templating/models.py
+++ templating/models.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 
 class Template(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     git_url = models.CharField(max_length=255, unique=True)
     author = models.ForeignKey(
@@ -13,3 +15,9 @@ class Template(models.Model):
     )
     published = models.BooleanField(default=False)
     lighthouse_score = models.IntegerField(default=0)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if not self.pk:
+            self.slug = slugify(self.name)
+        super().save(force_insert, force_update, using, update_fields)
